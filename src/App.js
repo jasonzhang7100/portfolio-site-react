b7c2b2a05bdc8a7eabaf8fdec3ab7639560ff977
Jason Zhang <jasonzhang@192-168-1-120.tpgi.com.au>
2021-05-21 15:51:29 +1000

    feat: done the structure design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function App() {
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </React.Fragment>
   );
-}
+};
 
 export default App;

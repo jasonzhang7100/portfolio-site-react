f920a71aacefdab92a80dc6c6068fcfe9e4e3eac
Jason Zhang <jasonzhang@192-168-1-120.tpgi.com.au>
2021-05-25 14:27:21 +1000

    feat: construct Projects component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,92 @@
 import React from "react";
+import {
+  makeStyles,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import clsx from "clsx";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-const Contact = () => {
-  return <div>Contact</div>;
-};
+import project1 from "../images/mern-stack.jpg";
 
-export default Contact;
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 300,
+  },
+  media: {
+    height: 0,
+    paddingTop: "80%",
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Pizza Order Project" />
+      <CardMedia className={classes.media} image={project1} title="Pizza Order" />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          It is a Pizza Order App created by React.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Stack:</Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add
+            chicken, shrimp and chorizo, and cook, stirring occasionally until lightly browned, 6 to 8
+            minutes.
+          </Typography>
+          <Typography paragraph>Add rice and stir very gently to distribute.</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,115 @@
 import React from "react";
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+
+import project1 from "../images/html-css-javascript-lg.jpg";
+import project2 from "../images/html-css-javascript.jpg";
+import project3 from "../images/javascript-fullstack.jpg";
+import project4 from "../images/mern-stack.jpg";
+import project5 from "../images/react-redux.jpg";
+import project6 from "../images/react.png";
+
+const useStyles = makeStyles({
+  cardContainer: {
+    maxWidth: 345,
+    margin: "3rem auto",
+  },
+});
+
+const projects = [
+  {
+    name: "Project 1",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+    consequatur magni quod nesciunt necessitatibus molestiae non
+    eligendi, magnam est aliquam recusandae? Magnam soluta minus
+    iste alias sunt veritatis nisi dolores!`,
+    image: project1,
+  },
+  {
+    name: "Project 2",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+    consequatur magni quod nesciunt necessitatibus molestiae non\
+    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+    iste alias sunt veritatis nisi dolores!`,
+    image: project2,
+  },
+  {
+    name: "Project 3",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+    consequatur magni quod nesciunt necessitatibus molestiae non\
+    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+    iste alias sunt veritatis nisi dolores!`,
+    image: project3,
+  },
+  {
+    name: "Project 4",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+    consequatur magni quod nesciunt necessitatibus molestiae non\
+    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+    iste alias sunt veritatis nisi dolores!`,
+    image: project4,
+  },
+  {
+    name: "Project 5",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+    consequatur magni quod nesciunt necessitatibus molestiae non\
+    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+    iste alias sunt veritatis nisi dolores!`,
+    image: project5,
+  },
+  {
+    name: "Project 6",
+    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
+    consequatur magni quod nesciunt necessitatibus molestiae non\
+    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
+    iste alias sunt veritatis nisi dolores!`,
+    image: project6,
+  },
+];
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const classes = useStyles();
+  return (
+    <Box component="div">
+      <Grid container justify="center">
+        {projects.map((project, index) => (
+          <Grid item xs={12} sm={6} lg={4} key={index}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea>
+                <CardMedia component="img" alt="Project 1" height="140" image={project.image} />
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                    {project.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {project.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Projects;
